5ac5561eb005748553336432cb04691c3f5a5b31
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2021-01-14 13:23:38 +0100

    updating intarget requirement to TP positioning, from EOI draft

--- tutorials/SNDLHC_Reader.C
+++ tutorials/SNDLHC_Reader.C
@@ -14,10 +14,10 @@ double GetParticleCharge (int pdgcode, TDatabasePDG *pdg){
 
 bool CheckNeutrinoVertexPosition(TVector3 nu_vertex){
   //target position (to select events in target - values need to be checked for each simulation)
-  const Double32_t targetxmin = 8.; 
-  const Double32_t targetxmax = 49.;
+  const Double32_t targetxmin = -47.6; 
+  const Double32_t targetxmax = -8.0;
   const Double32_t targetymin = 15.5;
-  const Double32_t targetymax = 56.5;
+  const Double32_t targetymax = 55.1;
      
   if (nu_vertex.X() > targetxmin && nu_vertex.X() < targetxmax && 
       nu_vertex.Y() > targetymin && nu_vertex.Y() < targetymax) return true;
@@ -75,7 +75,7 @@ void SNDLHC_Reader::Loop()
    TH2D *hvxy = new TH2D("hvxy","Position of neutrino interaction vertices in the transverse plane;x[cm];y[cm]", 200,-100,100,200,-100,100);
    TH2D *hvzy = new TH2D("hvzy","Position of neutrino interaction vertices in the zy plane;z[cm];y[cm]",60,-30,30,200,-100,100);
    //neutrino position only in target
-   TH2D *hvxy_intarget = new TH2D("hvxy_intarget","Position of neutrino interaction vertices in the transverse plane;x[cm];y[cm]", 100,0,100,100,0,100);
+   TH2D *hvxy_intarget = new TH2D("hvxy_intarget","Position of neutrino interaction vertices in the transverse plane;x[cm];y[cm]", 100,-100,0,100,0,100);
    TH2D *hvzy_intarget = new TH2D("hvzy_intarget","Position of neutrino interaction vertices in the zy plane;z[cm];y[cm]",60,-30,30,100,0,100);
    //muon filter position
    TH2D *hmuzy = new TH2D("hmuzy","Position of Muon Filter hits in the zy plane;z[cm];y[cm]",200,40,240,80,0,80);
