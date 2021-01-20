d9c27d43eaa3150d07a421eb9664fa9aad6523c0
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2021-01-20 22:37:07 +0100

    test to find a suitable variable for charm tau separation

--- nu_simulations/tauvscharm_Geniesims.C
+++ nu_simulations/tauvscharm_Geniesims.C
@@ -1,21 +1,33 @@
 //compare tau signal vs genie background
-double SumArray(ROOT::RVec<double> array){
+double SumArray(ROOT::RVec<double> array, ROOT::RVec<double> phiarray){
     double total = 0.;
-    for (auto &value:array){
-        total += value;
+    double maxphi = ROOT::VecOps::Max(abs(phiarray));
+    for (int i = 0; i < array.size(); i++){
+        if (phiarray[i] != maxphi) total += array[i]; //sum over all particle except maximum phi
     }
     return total;
 }
 
-double SumArray_NoCharm(ROOT::RVec<double> array, ROOT::RVec<int> pdgarray){
+double SumArray_NoCharm(ROOT::RVec<double> array, ROOT::RVec<int> pdgarray, ROOT::RVec<double> phiarray){
 //sum array content, excluding the one belonging to charm
     ROOT::RVec <int> charmpdgs = {421,411,4122,431, 4132, 4232, 4332, 441};
     double total = 0.;
     int ncharm=0;
+    double maxphi = 0; //a very small initial value
+    //finding maxphi (excluding charm)
+    for (int iparticle = 0; iparticle < pdgarray.size(); iparticle++){
+        int pdgcode = pdgarray[iparticle];
+        int ischarm = charmpdgs[charmpdgs == TMath::Abs(pdgcode)].size();
+        if (ischarm==0 && TMath::Abs(phiarray[iparticle]) > maxphi) maxphi = TMath::Abs(phiarray[iparticle]);
+    }
+    //doing the sum
     for (int i = 0; i < pdgarray.size(); i++){
         int pdgcode = pdgarray[i];
         int ischarm = charmpdgs[charmpdgs == TMath::Abs(pdgcode)].size();
-        if (ischarm == 0) total += array[i];
+        if (ischarm == 0){ 
+         if (maxphi != phiarray[i])
+          total += array[i]; //sum over all particle except maximum phi
+        }
         else ncharm++;
     }
     if (ncharm!=1) cout<<"Warning: only one charm expected, found "<<ncharm<<endl;
@@ -52,77 +64,99 @@ double angledifference(double ang1, double ang2){
   else delta = -2 * pi - delta; //negative, but less than 180°
   return delta;
 }
+
+ROOT::RVec<double> angledifferencewitharray(double ang1, ROOT::RVec<double> ang2array){
+  //it cannot go over pi, I need to take the shortest one
+  ROOT::RVec<double> deltaarray;
+  double delta;
+  for (auto & ang2:ang2array){
+   delta = angledifference(ang1,ang2);
+   deltaarray.push_back(delta);
+  }
+  return deltaarray;
+}
+
 //get array content for charm
 void tauvscharm_Geniesims(){
     //****************************tau (signal)***************************************
     ROOT::RDataFrame dftau("gst","/home/utente/Simulations/sim_snd/GenieEvents_SNDAcceptance/CCDIS/nutau/nutau_CCDIS_FairShip.root"); 
 
+    auto dftau1 = dftau.Define("phil","TMath::ATan2(pyl,pxl)");
+
+    //computing phi for each particle, deltaphi with respect to charm/tau
+    auto dftau2 = dftau1.Define("phif","atan2(pyf,pxf)");
+
+    auto dftau3 = dftau2.Define("deltaphif",angledifferencewitharray,{"phil","phif"});
+
     //vector sum per components of final hadronic momentum
-    auto dftau1 = dftau.Define("pxf_tot",SumArray,{"pxf"})
-                       .Define("pyf_tot",SumArray,{"pyf"})
-                       .Define("pzf_tot",SumArray,{"pzf"});
+    auto dftau4 = dftau3.Define("pxf_tot",SumArray,{"pxf","deltaphif"})
+                        .Define("pyf_tot",SumArray,{"pyf","deltaphif"})
+                        .Define("pzf_tot",SumArray,{"pzf","deltaphif"});
 
     //getting angles
-    auto dftau2 = dftau1.Define("txl","pxl/pzl")
+    auto dftau5 = dftau4.Define("txl","pxl/pzl")
                         .Define("tyl","pyl/pzl");
-    auto dftau3 = dftau2.Define("txf","pxf/pzf")
+    auto dftau6 = dftau5.Define("txf","pxf/pzf")
                         .Define("tyf","pyf/pzf");
 
     //sum of angles for final hadronic state
-    auto dftau4 = dftau3.Define("txf_tot",SumArray,{"txf"})
-                        .Define("tyf_tot",SumArray,{"tyf"});
+    auto dftau7 = dftau6.Define("txf_tot",SumArray,{"txf","deltaphif"})
+                        .Define("tyf_tot",SumArray,{"tyf","deltaphif"});
 
     //getting ideal phi of final hadronic system in the ideal case of momenta
-    auto dftau5 = dftau4.Define("phif_tot","TMath::ATan2(pyf_tot,pxf_tot)");
+    auto dftau8 = dftau7.Define("phif_tot","TMath::ATan2(pyf_tot,pxf_tot)");
     //getting ideal phi of tau lepton
-    auto dftau6 = dftau5.Define("phil","TMath::ATan2(pyl,pxl)");
     //deltaphi
-    auto dftau7 = dftau6.Define("deltaphi",angledifference,{"phil","phif_tot"});
+    auto dftau9 = dftau8.Define("deltaphi",angledifference,{"phil","phif_tot"});
 
     //getting detected phi (phi_det) of tau lepton from angles
-    auto dftau8 = dftau7.Define("phil_det","TMath::ATan2(tyl,txl)");
+    auto dftau10 = dftau9.Define("phil_det","TMath::ATan2(tyl,txl)");
     //getting detected phi from angles of final hadronic system
-    auto dftau9 = dftau8.Define("phif_tot_det","TMath::ATan2(tyf_tot, txf_tot)");
+    auto dftau11 = dftau10.Define("phif_tot_det","TMath::ATan2(tyf_tot, txf_tot)");
     //deltaphi
-    auto dftau_final = dftau9.Define("deltaphi_det",angledifference,{"phil_det","phif_tot_det"});
+    auto dftau_final = dftau11.Define("deltaphi_det",angledifference,{"phil_det","phif_tot_det"});
 
     //****************************charm (background) (charm is NOT always pdgf[0], we add a step to find its momenta)*********************************
     ROOT::RDataFrame dfcharm("gst","/home/utente/Simulations/sim_snd/GenieEvents_SNDAcceptance/CharmCCDIS/numu/numu_CharmCCDIS_FairShip.root");
+
+    auto dfcharm0 = dfcharm.Define("phif","atan2(pyf,pxf)");
     //find charm momentum 
-    auto dfcharm0 = dfcharm.Define("pxcharm",GetCharmValue,{"pxf","pdgf"})
-                           .Define("pycharm",GetCharmValue,{"pyf","pdgf"})
-                           .Define("pzcharm",GetCharmValue,{"pzf","pdgf"})
-                           .Define("pdgcharm",GetCharmValueInt,{"pdgf","pdgf"});                        
+    auto dfcharm1 = dfcharm0.Define("pxcharm",GetCharmValue,{"pxf","pdgf"})
+                               .Define("pycharm",GetCharmValue,{"pyf","pdgf"})
+                               .Define("pzcharm",GetCharmValue,{"pzf","pdgf"})
+                               .Define("pdgcharm",GetCharmValueInt,{"pdgf","pdgf"});              
+    //getting ideal phi of charm
+    auto dfcharm2 = dfcharm1.Define("phicharm","TMath::ATan2(pycharm,pxcharm)");
+    
+    auto dfcharm3 = dfcharm2.Define("deltaphif",angledifferencewitharray,{"phicharm","phif"}); 
     //vector sum per components of final hadronic momentum
-    auto dfcharm1 = dfcharm0.Define("pxf_tot",SumArray_NoCharm,{"pxf","pdgf"})
-                           .Define("pyf_tot",SumArray_NoCharm,{"pyf","pdgf"})
-                           .Define("pzf_tot",SumArray_NoCharm,{"pzf","pdgf"});
+    auto dfcharm4 = dfcharm3.Define("pxf_tot",SumArray_NoCharm,{"pxf","pdgf","deltaphif"})
+                               .Define("pyf_tot",SumArray_NoCharm,{"pyf","pdgf","deltaphif"})
+                               .Define("pzf_tot",SumArray_NoCharm,{"pzf","pdgf","deltaphif"});
 
     //getting angles
-    auto dfcharm2 = dfcharm1.Define("txcharm","pxcharm/pzcharm")
+    auto dfcharm5 = dfcharm4.Define("txcharm","pxcharm/pzcharm")
                             .Define("tycharm","pycharm/pzcharm");
-    auto dfcharm3 = dfcharm2.Define("txf","pxf/pzf")
+    auto dfcharm6 = dfcharm5.Define("txf","pxf/pzf")
                             .Define("tyf","pyf/pzf");
-    auto dfcharm3bis = dfcharm3.Define("txl","pxl/pzl")
+    auto dfcharm7 = dfcharm6.Define("txl","pxl/pzl")
                              .Define("tyl","pyl/pzl");
                             
     //sum of angles for final hadronic state
-    auto dfcharm4 = dfcharm3bis.Define("txf_tot",SumArray_NoCharm,{"txf","pdgf"})
-                            .Define("tyf_tot",SumArray_NoCharm,{"tyf","pdgf"});
+    auto dfcharm8 = dfcharm7.Define("txf_tot",SumArray_NoCharm,{"txf","pdgf","deltaphif"})
+                               .Define("tyf_tot",SumArray_NoCharm,{"tyf","pdgf","deltaphif"});
 
     //getting ideal phi of final hadronic system in the ideal case of momenta
-    auto dfcharm5 = dfcharm4.Define("phif_tot","TMath::ATan2(pyf_tot,pxf_tot)");
-    //getting ideal phi of charm
-    auto dfcharm6 = dfcharm5.Define("phicharm","TMath::ATan2(pycharm,pxcharm)");
+    auto dfcharm9 = dfcharm8.Define("phif_tot","TMath::ATan2(pyf_tot,pxf_tot)");
     //deltaphi
-    auto dfcharm7 = dfcharm6.Define("deltaphi",angledifference,{"phicharm","phif_tot"});
+    auto dfcharm10 = dfcharm9.Define("deltaphi",angledifference,{"phicharm","phif_tot"});
 
     //getting detected phi (phi_det) of charm lepton
-    auto dfcharm8 = dfcharm7.Define("phicharm_det","TMath::ATan2(tycharm,txcharm)");
+    auto dfcharm11 = dfcharm10.Define("phicharm_det","TMath::ATan2(tycharm,txcharm)");
     //getting detected phi of final hadronic system
-    auto dfcharm9 = dfcharm8.Define("phif_tot_det","TMath::ATan2(tyf_tot, txf_tot)");
+    auto dfcharm12 = dfcharm11.Define("phif_tot_det","TMath::ATan2(tyf_tot, txf_tot)");
     //deltaphi
-    auto dfcharm_final = dfcharm9.Define("deltaphi_det",angledifference,{"phicharm_det","phif_tot_det"});
+    auto dfcharm_final = dfcharm12.Define("deltaphi_det",angledifference,{"phicharm_det","phif_tot_det"});
 
     //drawing histograms
     auto hdeltaphitau = dftau_final.Define("deltaphi_deg","deltaphi * TMath::RadToDeg()")
