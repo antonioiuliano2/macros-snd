0a2fe4867265136770b18d7672221956dd8e1902
antonioiuliano2 <antonio.iuliano1993@gmail.com>
2021-01-20 22:33:56 +0100

    separating functions with subcommands

--- makeSNDGenieEvents.py
+++ makeSNDGenieEvents.py
@@ -3,13 +3,17 @@ import ROOT,os,sys,time
 import argparse
 import genie_interface
 
-defaultfiledir = '/afs/cern.ch/work/a/aiuliano/public/Generate_GENIEinput_AnnaritaSND/NeutrinoFiles/'
-defaultsplinedir = '/afs/cern.ch/work/a/aiuliano/public/Generate_GENIEinput_AnnaritaSND/SplinesTungstenTP/'
+defaultfiledir = '/eos/user/a/aiuliano/public/sims_FairShip/sim_snd/Generate_GENIEinput_AnnaritaSND/NeutrinoFiles/'
+defaultsplinedir = '/eos/user/a/aiuliano/public/sims_FairShip/sim_snd/Generate_GENIEinput_AnnaritaSND/SplinesTungstenTP/'
 names = {14:'numu',12:'nue', 16:'nutau', -14:'anumu',-12:'anue', -16:'anutau'}
 filenames = {14:'NeutMuon',12:'NeutElec',16:'NeutTau_filter',-14:'AntiNeutMuon',-12:'AntiNeutElec',-16:'AntiNeutTau_filter'}
 
 def init(): #available options
-    ap = argparse.ArgumentParser(description='Run GENIE neutrino simulation')
+    parser = argparse.ArgumentParser(description='Run GENIE neutrino simulation')
+    subparsers = parser.add_subparsers()
+
+    ap = subparsers.add_parser('sim', help="make genie simulation file")
+    
     ap.add_argument( '--nupdg', type=str, dest='nupdg', default=None)
     ap.add_argument('-n', '--nevents', type=int, dest='nevents', default=1000)
     ap.add_argument('-f','--filedir', type=str, help="directory with neutrino fluxes", dest='filedir', default=defaultfiledir)
@@ -18,32 +22,25 @@ def init(): #available options
     ap.add_argument('-p', '--process', type=str, help='which interaction process',dest='process', default=None)
     ap.add_argument('-s', '--seed', type=int, dest='seed', default=65539) #default seed in $GENIE/src/Conventions/Controls.h
     ap.add_argument('-t', '--target', type=str, help="target material", dest='target', default='tungstenTP')
-    ap.add_argument('--FS', help="produce ntuples for FairShip", dest='FairShip', action="store_true")
-    ap.add_argument('--MS', help="make a new spline file", dest='MakeSpline', action="store_true")
-    args = ap.parse_args()
+
+    ap1 = subparsers.add_parser('spline', help="make a new cross section spline file")
+    ap1.add_argument('-t', '--target', type=str, help="target material", dest='target', default='tungstenTP')
+    ap1.add_argument('-o', '--output',type=str, help="output directory", dest='outdir', default=None)
+    args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
     args = init()
     nupdg = int(args.nupdg)
-    nevents = int(args.nevents)
-
-    process = args.process
-    target = args.target
-    seed = args.seed
-
-    outdir = args.outdir
-    filedir = args.filedir
-    splinedir = args.splinedir
 
     FairShip = args.FairShip
     MakeSpline = args.MakeSpline
 
     print('Neutrino PDG code: ', nupdg)
     print('Number of events to generate: ', nevents)
-    print('Process to simulate: ', process)
-    print('Target type: ', target)
-    print('Seed used in this generation: ', seed)
+    print('Process to simulate: ', args.process)
+    print('Target type: ', args.target)
+    print('Seed used in this generation: ', args.seed)
 
     if 'GALGCONF' not in os.environ:
         sys.exit('GALGCONF is not set to a conf folder: need to configure GENIE for SND high energies!')        
@@ -52,28 +49,28 @@ if __name__ == '__main__':
         print('Please specify the neutrino type!')
         sys.exit('Aborting code')
     
-    if target=='tungstenTP':
+    if args.target=='tungstenTP':
         targetcode = '1000741840[0.95],1000280580[0.03],1000290630[0.02]'
-    elif target=='tungstenEOI':
+    elif args.target=='tungstenEOI':
         targetcode = '1000741840[0.9],1000280580[0.1]'
     else:
         print('no other cross-sections available!')
 
     if (FairShip):
 
-  
-     if process==None:
+     nevents = int(args.nevents)
+     if args.process==None:
         print('no process selected, generating with default GENIE processes')
 
      #Getting input flux and spline path
-     inputfile = filedir+filenames[nupdg]+'.root'
-     spline = splinedir+names[nupdg]+'_xsec_splines.xml'
+     inputfile = args.filedir+filenames[nupdg]+'.root'
+     spline = args.splinedir+names[nupdg]+'_xsec_splines.xml'
      #setting path of outputfile
-     outputfile = outdir+names[nupdg]+"_"+process+"_FairShip.root"
+     outputfile = args.outdir+names[nupdg]+"_"+args.process+"_FairShip.root"
      #generating GENIE simulation
      genie_interface.generate_genie_events(nevents = nevents, nupdg = nupdg, emin = 0, emax = 5000, \
                                        targetcode = targetcode, inputflux = inputfile, \
-                                        spline = spline, process = process )
+                                        spline = spline, process = args.process, seed = args.seed)
      #converting GENIE simulation into FairShip compatible format
      genie_interface.make_ntuples("gntp.0.ghep.root", outputfile)
 
@@ -82,7 +79,7 @@ if __name__ == '__main__':
 
     elif (MakeSpline):
      #setting path of outputfile
-     outputfile = outdir+names[nupdg]+'_xsec_splines.xml'
+     outputfile = args.outdir+names[nupdg]+'_xsec_splines.xml'
      #generating new set of splines, saving them in outputdir
      nupdglist = [nupdg]
      genie_interface.make_splines(nupdglist = nupdglist,targetcode = targetcode,emax = 5000, nknots = 100, outputfile = outputfile)
