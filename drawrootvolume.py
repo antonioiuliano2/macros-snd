b725c758e0ee8b814037606922397fbcd7fc23da
AntonioIuliano <antonio.iuliano@cern.ch>
2021-10-11 14:10:16 +0200

    following Thomas advice on findvolumefast

--- drawrootvolume.py
+++ drawrootvolume.py
@@ -2,5 +2,6 @@ import ROOT as r
 import sys
 #usage: python -i drawrootvolume.py geofile.root
 r.TGeoManager.Import(sys.argv[1])
+#r.gGeoManager.FindVolumeFast("volTarget").Draw("ogl")
 r.gGeoManager.GetTopVolume().Draw("ogl")#interesting volumes: Wall, volTarget, Cell...
 r.gGeoManager.SetVisLevel(9);
