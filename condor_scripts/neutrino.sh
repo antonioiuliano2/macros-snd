b1f08d91df3c227ac54f928da41909007486235e
aiulian <antonio.iuliano@cern.ch>
2021-11-12 14:17:23 +0100

    updating file names

--- condor_scripts/neutrino.sh
+++ condor_scripts/neutrino.sh
@@ -8,18 +8,19 @@ echo $LSB_JOBINDEX
 
 sleep $ProcId
 
-SNDLHC_mymaster=/afs/cern.ch/work/a/aiuliano/public/SNDLHCBuild
+SNDLHC_mymaster=/afs/cern.ch/work/a/aiulian/public/SNDLHCBuild
 export ALIBUILD_WORK_DIR=$SNDLHC_mymaster/sw #for alienv
 
 echo "SETUP"
-source /cvmfs/ship.cern.ch/SHiP-2021/latest/setUp.sh
+source /cvmfs/sndlhc.cern.ch/latest/setUp.sh
 eval `alienv load sndsw/latest`
 
 echo "Starting script, from neutrino number "
 echo $STARTEVENT 
-INPUTFILES=/afs/cern.ch/work/a/aiuliano/public/Generate_GENIEinput_AnnaritaSND/GenieEventsTP/
+INPUTFILES=/afs/cern.ch/work/a/aiulian/public/genie_sims
 echo "From file "
 echo $INPUTFILES
 
-OUTPUTDIR=/afs/cern.ch/work/a/aiuliano/public/sim_snd/nueNCDIS_SND_7_January_2020
-/cvmfs/ship.cern.ch/SHiP-2021/2020/November/16/sw/slc7_x86-64/Python/v3.6.8-1/bin/python3 $SNDLHC_mymaster/sndsw/macro/run_simScript.py --shiplhc --Genie -f $INPUTFILES/NCDIS/nue/nue_NCDIS_FairShip.root -i $STARTEVENT -n $NEVENTS -o $OUTPUTDIR/$LSB_JOBINDEX
+OUTPUTDIR=/afs/cern.ch/work/a/aiulian/public/sim_snd/condor_sims/numu_sim_activeemu_10_November_2021
+
+/cvmfs/sndlhc.cern.ch/SNDLHC-2021/June/15/sw/slc7_x86-64/Python/v3.6.8-local1/bin/python3 $SNDLHC_mymaster/sndsw/shipLHC/run_simSND.py --Genie 1 -f /afs/cern.ch/work/a/aiulian/public/genie_sims/GenieEvents_3_06/CCDIS/numu_CCDIS_FairShip_withFLUKA.root -i $STARTEVENT -n $NEVENTS -o $OUTPUTDIR/$LSB_JOBINDEX
