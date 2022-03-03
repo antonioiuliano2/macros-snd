2e275d6c7a82fffade596027ae4499c8dc668417
antonioiuliano2 <antonio.iuliano@cern.ch>
2022-03-03 15:30:32 +0100

    using set information for taking care of missing patterns

--- FEDRA/doreco.sh
+++ FEDRA/doreco.sh
@@ -8,7 +8,7 @@ for ibrick in $(seq 0 19)
  do
   echo "processing brick ${brickIDs[ibrick]}"
   cd b0000${brickIDs[ibrick]}
-  echo "makescanset -set=${brickIDs[ibrick]}.0.0.0 -from_plate=60 -to_plate=1 -dz=-1315 -suff=cp.root" >> scanset.sh
+  echo "makescanset -set=${brickIDs[ibrick]}.0.0.0 -from_plate=60 -to_plate=1 -dz=-1315 -suff=cp.root" > scanset.sh
   source scanset.sh
   cp ../track.rootrc ./
   ncouples=$(root -l -q ../GetEntries.C\(${brickIDs[ibrick]}\))
@@ -20,6 +20,6 @@ for ibrick in $(seq 0 19)
   emtra -set=${brickIDs[ibrick]}.0.0.0 -new -v=2
   ln -s b0000${brickIDs[ibrick]}.0.0.0.trk.root linked_tracks.root
   cp /home/simsndlhc/macros-snd/FEDRA/vertexing.C ./
-  root -l -q vertexing.C
+  root -l -q vertexing.C\(${brickIDs[ibrick]}\)
   cd ..
  done
