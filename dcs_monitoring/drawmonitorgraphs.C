01e03fb039070aceeb5fe1e99d7913b10da4845e
aiulian <antonio.iuliano@cern.ch>
2022-04-25 21:03:40 +0200

    fixing scale issue

--- dcs_monitoring/drawmonitorgraphs.C
+++ dcs_monitoring/drawmonitorgraphs.C
@@ -26,6 +26,9 @@ void updatemonitorgraphs(const char* inputfilename, TFile *graphfile){
         tempgraphs[isensor] = new TGraph();
         humiditygraphs[isensor] = new TGraph();
 
+        tempgraphs[isensor]->SetName(Form("tempgraph_%i",isensor+1));
+        humiditygraphs[isensor]->SetName(Form("humiditygraph_%i",isensor+1));
+
         tempgraphs[isensor]->SetTitle(Form("Temperature for sensor %i;Time[H:M];temp[C]",isensor+1));
         humiditygraphs[isensor]->SetTitle(Form("Humidity for sensor %i;Time[H:M];rh",isensor+1));
     }
@@ -48,18 +51,20 @@ void updatemonitorgraphs(const char* inputfilename, TFile *graphfile){
        ctrh->cd(1);
        tempgraphs[isensor]->GetXaxis()->SetTimeDisplay(1);
        tempgraphs[isensor]->GetXaxis()->SetTimeFormat("%d/%m-%H:%M");
+       tempgraphs[isensor]->SetLineColor(colors[isensor]);
+       tempgraphs[isensor]->SetMinimum(10);
+       tempgraphs[isensor]->SetMaximum(30);
        if (isensor > 0) tempgraphs[isensor]->Draw("SAME");
        else tempgraphs[isensor]->Draw();
-       tempgraphs[isensor]->SetLineColor(colors[isensor]);
-       tempgraphs[isensor]->GetYaxis()->SetRangeUser(10,20);
 
        ctrh->cd(2);
        humiditygraphs[isensor]->GetXaxis()->SetTimeDisplay(1);
        humiditygraphs[isensor]->GetXaxis()->SetTimeFormat("%d/%m-%H:%M");
+       humiditygraphs[isensor]->SetLineColor(colors[isensor]);
+       humiditygraphs[isensor]->SetMinimum(30);
+       humiditygraphs[isensor]->SetMaximum(60);
        if (isensor > 0) humiditygraphs[isensor]->Draw("SAME");
        else humiditygraphs[isensor]->Draw();
-       humiditygraphs[isensor]->SetLineColor(colors[isensor]);
-       humiditygraphs[isensor]->GetYaxis()->SetRangeUser(30,60);
     }
     ctrh->GetPad(1)->BuildLegend();
     ctrh->GetPad(2)->BuildLegend();
@@ -77,18 +82,17 @@ void updatemonitorgraphs(const char* inputfilename, TFile *graphfile){
     graphfile->cd();
     ctrh->SetName("ctrh");
     ctrh->Write("ctrh",TObject::kOverwrite);
+    graphfile->SaveSelf();
 
 }
 
 void drawmonitorgraphs(const char *inputfilename){
     TString graphfilename = TString("/eos/experiment/sndlhc/www/online/graphs_"+TString(inputfilename));
 
-    TFile *graphfile = new TFile(graphfilename.Data(),"UPDATE"); //does not delete existing data
-    TUnixSystem *mysystem = new TUnixSystem();
-    while(1){
-         //cout<<"Updated graphs"<<endl;
+    TFile *graphfile = new TFile(graphfilename.Data(),"RECREATE");
+    while(1){        
          updatemonitorgraphs(inputfilename,graphfile); //does not stop on its own, only when killed
-         graphfile->SaveSelf();
-         mysystem->Sleep(1800*1000.);
+         cout<<"Updated graphs"<<endl;
+         gSystem->Sleep(1800*1000.); //every 30 minutes
         }
 }
