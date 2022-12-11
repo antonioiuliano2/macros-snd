df8e16ff8fc2243235ce3830bd89d470665b4270
scanner <antonio.iuliano@unina.it>
2022-12-11 16:52:17 +0100

    to avoid gaps in middle, require only starting point of tracks in quarter

--- run0analysis/rdataframe_anglestracks.C
+++ run0analysis/rdataframe_anglestracks.C
@@ -54,7 +54,7 @@ EdbSegP GetLastSegment(TClonesArray sf){
 
 using namespace ROOT;
 void rdataframe_anglestracks(){
-  TFile *tracksfile = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/trackfiles/rootfiles/upstream25_v0_2/fourthquarter_upstream25_trk.root");
+  TFile *tracksfile = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/trackfiles/rootfiles/upstream25_v0_2/thirdquarter_upstream25_trk.root");
   TTree *trackstree = (TTree*) tracksfile->Get("tracks");
   //FOR MIC2 (use these if last two plates are scanned from mic2)!
   const double txslant = 0.0125672;
@@ -105,7 +105,8 @@ void rdataframe_anglestracks(){
   auto dftr2 = dftr1.Define("tantheta","tr.Theta()").Define("theta","TMath::ATan(tantheta)");
 
   //selecting good tracks (aka long, in this case
-  auto dfgoodtr = dftr2.Filter(Form("nseg>=%i",minnseg)).Filter("Xcm>9.6&&Ycm>9.6&&Xcmlast>9.6&&Ycmlast>9.6"); //cut in position
+  //auto dfgoodtr = dftr2.Filter(Form("nseg>=%i",minnseg)).Filter("Xcm>9.6&&Ycm>9.6&&Xcmlast>9.6&&Ycmlast>9.6"); //cut in position
+  auto dfgoodtr = dftr2.Filter(Form("nseg>=%i",minnseg)).Filter("Xcm<9.6&&Ycm>9.6"); //cut in position
   //auto dfgoodtr = dftr2.Filter(Form("nseg>=%i",minnseg)); //not cut in position
 
   auto hxy = dfgoodtr.Histo2D({"hxy","xy map;x[cm];y[cm]",nbinsx,xmin,xmax,nbinsy,ymin,ymax},"Xcm","Ycm");
@@ -130,7 +131,7 @@ void rdataframe_anglestracks(){
   //just for testing COV
   auto hvarx = dfgoodtr.Histo1D("varx");
 
-  TFile *canvasfile = new TFile("plots/plots_25plates_fourthquarter_witheffcorrection.root","RECREATE");
+  TFile *canvasfile = new TFile("plots/plots_25plates_thirdquarter_witheffcorrection_onlystarting.root","RECREATE");
   canvasfile->cd();
   //Drawing plots
   TCanvas *ctx = new TCanvas("ctx","TX Canvas",1600,800);
@@ -238,15 +239,15 @@ void rdataframe_anglestracks(){
 
 //merging different quarters together, making plot and mean
 void merge_quarters(){
- TFile * tracks1 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_firstquarter_witheffcorrection.root");
- TFile * tracks2 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_secondquarter_witheffcorrection.root");
- TFile * tracks3 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_thirdquarter_witheffcorrection.root");
- TFile * tracks4 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_fourthquarter_witheffcorrection.root");
+ TFile * tracks1 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_firstquarter_witheffcorrection_onlystarting.root");
+ TFile * tracks2 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_secondquarter_witheffcorrection_onlystarting.root");
+ TFile * tracks3 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_thirdquarter_witheffcorrection_onlystarting.root");
+ TFile * tracks4 = TFile::Open("/home/scanner/sndlhc/RUN0/b000131/plots/plots_25plates_fourthquarter_witheffcorrection_onlystarting.root");
 
- TH2D *hxy_effcorrected_incompletebeta_1 = (TH2D*) tracks1->Get("hxy_effcorrected_incompletebeta");
- TH2D *hxy_effcorrected_incompletebeta_2 = (TH2D*) tracks2->Get("hxy_effcorrected_incompletebeta");
- TH2D *hxy_effcorrected_incompletebeta_3 = (TH2D*) tracks3->Get("hxy_effcorrected_incompletebeta");
- TH2D *hxy_effcorrected_incompletebeta_4 = (TH2D*) tracks4->Get("hxy_effcorrected_incompletebeta");
+ TH2D *hxy_effcorrected_incompletebeta_1 = (TH2D*) tracks1->Get("hxy_effcorrected");
+ TH2D *hxy_effcorrected_incompletebeta_2 = (TH2D*) tracks2->Get("hxy_effcorrected");
+ TH2D *hxy_effcorrected_incompletebeta_3 = (TH2D*) tracks3->Get("hxy_effcorrected");
+ TH2D *hxy_effcorrected_incompletebeta_4 = (TH2D*) tracks4->Get("hxy_effcorrected");
  //adding them together
  hxy_effcorrected_incompletebeta_1->Add(hxy_effcorrected_incompletebeta_2);
  hxy_effcorrected_incompletebeta_1->Add(hxy_effcorrected_incompletebeta_3);
@@ -254,14 +255,18 @@ void merge_quarters(){
 
  //making the average value
  const int nbinsx = 19;
- const int minbin = 3;
- const int maxbin = nbinsx-3;
- ROOT::RVec<int> nmuons_array;
+ const int nbinsy = 19;
+ const int minbinx = 3;
+ const int maxbinx = nbinsx-3;
+ const int minbiny = 3;
+ const int maxbiny = nbinsy-3;
+ 
+ ROOT::RVec<double> nmuons_array;
 
- for (int ibinx = minbin+1; ibinx <=maxbin; ibinx++){ //bin 0 is actually underflow!
-   for (int ibiny = minbin+1; ibiny <=maxbin; ibiny++){
-    int nmuons = hxy_effcorrected_incompletebeta_1->GetBinContent(ibinx,ibiny);
-    nmuons_array.push_back(nmuons);
+ for (int ibinx = 1; ibinx <=nbinsx; ibinx++){ //bin 0 is actually underflow!
+   for (int ibiny = 1; ibiny <=nbinsx; ibiny++){
+    double nmuons = hxy_effcorrected_incompletebeta_1->GetBinContent(ibinx,ibiny);
+    if (ibinx >= minbinx && ibinx <= maxbinx && ibiny >= minbiny && ibiny <= maxbiny) nmuons_array.push_back(nmuons);
     //only for visualization, to closest integer
     hxy_effcorrected_incompletebeta_1->SetBinContent(ibinx,ibiny,TMath::Nint(nmuons));
    }
@@ -272,19 +277,19 @@ void merge_quarters(){
 
  hxy_effcorrected_incompletebeta_1->Draw("COLZ && TEXT");
 
- TLine *lx0 = new TLine(minbin,minbin,minbin,maxbin);
+ TLine *lx0 = new TLine(minbinx-1,minbiny-1,minbinx-1,maxbiny+1);
  lx0->SetLineColor(kRed);
  lx0->SetLineWidth(3);
 
- TLine *lx1 = new TLine(maxbin,minbin,maxbin,maxbin);
+ TLine *lx1 = new TLine(maxbinx+1,minbiny-1,maxbinx+1,maxbiny+1);
  lx1->SetLineColor(kRed);
  lx1->SetLineWidth(3);
  
- TLine *ly0 = new TLine(minbin,minbin,maxbin,minbin);
+ TLine *ly0 = new TLine(minbinx-1,minbiny-1,maxbinx+1,minbiny-1);
  ly0->SetLineColor(kRed);
  ly0->SetLineWidth(3);
 
- TLine *ly1 = new TLine(minbin,maxbin,maxbin,maxbin);
+ TLine *ly1 = new TLine(minbinx-1,maxbiny+1,maxbinx+1,maxbiny+1);
  ly1->SetLineColor(kRed);
  ly1->SetLineWidth(3);
 
@@ -293,7 +298,7 @@ void merge_quarters(){
  ly0->Draw("SAME");
  ly1->Draw("SAME");
 
- cout<<"N values "<<nmuons_array.size()<<endl;
+ cout<<"N values "<<nmuons_array.size()<<endl; 
  cout<<"Valor Medio: "<<ROOT::VecOps::Mean(nmuons_array)<<" con errore "<<ROOT::VecOps::StdDev(nmuons_array)/TMath::Sqrt(nmuons_array.size())<<endl;
 
  
