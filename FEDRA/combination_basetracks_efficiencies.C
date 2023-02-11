5914f9d87d37562f16ba54adb2cc3c96c2ffdbf2
scanner <antonio.iuliano@unina.it>
2023-02-11 11:54:09 +0100

    updating code for efficiency measurements, more clear and easier to generalize

--- FEDRA/combination_basetracks_efficiencies.C
+++ FEDRA/combination_basetracks_efficiencies.C
@@ -14,19 +14,27 @@ vector<int> people;
 vector<int> combination;
 
 int countiterations = 0;
-int iplateoffset = 6; //I did not start from 1 to 25, but from 7 (just for printing)
 
-const int nfilms = 25;
+
+const int nfilms = 25; //number of films
+const int firstfilm = 15; //starting from
+const int lastfilm = 39; //ending into
+//should we take into account holes?
+const int min_nfilms = 12; //how many films we require
+
+TString eff_filename("efficiency_plate_RUN1_onesquarecm_39_15.root");
+
 double plateefficiency[nfilms];
 
 void SetEfficiencyArray(){
     //TFile *inputfile = TFile::Open("efficiency_plate_requiredallfilms.root");
-    TFile *inputfile = TFile::Open("efficiency_plate_allquarters.root");
+    TFile *inputfile = TFile::Open(eff_filename.Data());
     TEfficiency *heffplates = (TEfficiency*) inputfile->Get("hexpected_clone");
     cout<<"Starting code, getting efficiency from tgraph"<<endl;
     for (int ifilm = 0; ifilm < nfilms; ifilm++){
-        plateefficiency[ifilm] = heffplates->GetEfficiency(iplateoffset+2+ifilm);
-        cout<<"Efficiency for film "<<ifilm+iplateoffset+1<<": "<<plateefficiency[ifilm]<<endl;
+        int nfilm = ifilm + firstfilm;
+        plateefficiency[ifilm] = heffplates->GetEfficiency(nfilm+1); //first bin is 0
+        cout<<"Efficiency for film "<<nfilm<<": "<<plateefficiency[ifilm]<<endl;
     }
 }
 
@@ -34,41 +42,11 @@ double pevent(const vector<int>&combination){
  //probability of having a particular combination of base-tracks
  //if base-track in a plate is there, multiply for eff[film], otherwise multiply for 1-eff[film]   
  double prob = 1.;
- const int nfilms = 25;
- /*
- double plateefficiency[nfilms]{
-    0.920287,
-    0.892932,
-    0.930555,
-    0.917941,
-    0.95256,
-    0.957046,
-    0.943092,
-    0.966633,
-    0.877558,
-    0.962151,
-    0.97265,
-    0.970084,
-    0.968353,
-    0.970078,
-    0.939952,
-    0.88511,
-    0.968246,
-    0.925841,
-    0.944591,
-    0.813822,
-    0.933855,
-    0.942422,
-    0.882333,
-    0.909087,
-    0.838794,
-  };
-  */
  bool filmfound[nfilms]; //will tell us if film contains a basetrack
  std::fill_n(filmfound,nfilms,false); //should be already false by default, but let us initialize it for safety
  //looping over all films, storing the ones "found" in the combination
  for (int iplate:combination){
-    filmfound[iplate-1] = true; //plates number start from 1, array elements from 0
+    filmfound[iplate] = true; 
  }
  for (int ifilm = 0; ifilm < nfilms; ifilm++){
     if (filmfound[ifilm]) prob = prob * plateefficiency[ifilm]; //plate was found, p;
@@ -81,7 +59,7 @@ double pevent(const vector<int>&combination){
 void pretty_print(const vector<int>& v) {
   //static int count = 0;
   cout << "combination no " << (++countiterations) << ": [ ";
-  for (int i = 0; i < v.size(); ++i) { cout << v[i] + iplateoffset << " "; }
+  for (int i = 0; i < v.size(); ++i) { cout << v[i] + firstfilm << " "; }
   cout << "] ";
 }
 double totprob = 0.; //global variabile, reporting total probability
@@ -103,10 +81,11 @@ void go(int offset, int k) {
 }
 
 int main() {
-  const int n = 25, mink = 18;
+  int n = nfilms;
+  int mink = min_nfilms;
   SetEfficiencyArray();
   double probiteration = 0;
-  for (int i = 0; i < n; ++i) { people.push_back(i+1); }
+  for (int i = 0; i < n; ++i) { people.push_back(i); }
   for (int k = mink; k <= n; ++k) {
     cout<<"Starting combinations of "<<k<<" plates over "<<n<<endl;
     go(0, k);countiterations = 0;
