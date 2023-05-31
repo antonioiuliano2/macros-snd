809406af387891f8e00f7524cde9695bc79169ba
dcentanni <daniele.centanni@outlook.it>
2023-05-31 17:45:44 +0200

    added condor cell tracking

--- condor_scripts/condortracking_realign.sub.sh
+++
@@ -1,8 +0,0 @@
-executable = condortraking_realign.sh
-arguments = $(ClusterID) $(ProcId) $(CELL)
-output = output/trackingrealign.$(ClusterId).$(ProcId).out
-error = error/trackingrealign.$(ClusterId).$(ProcId).err
-log = log/trackingrealign.$(ClusterId).log
-+JobFlavour = "workday"
-notification = Never
-queue CELL from seq 1 360 |
