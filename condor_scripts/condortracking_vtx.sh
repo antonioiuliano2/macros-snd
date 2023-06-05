8a8e69e2cc74d95abce80113749e5ae47a188ea8
snd2cern_antonio <antonio.iuliano@cern.ch>
2023-06-05 11:36:00 +0200

    fixing linux eos and paths

--- condor_scripts/condortracking_vtx.sh
+++ condor_scripts/condortracking_vtx.sh
@@ -6,10 +6,11 @@ CELL=$3
 
 
 echo "Set up SND environment"
-SNDBUILD_DIR=/afs/cern.ch/work/a/aiulian/public/SNDLHCBuild/sw/
-source /cvmfs/sndlhc.cern.ch/SNDLHC-2022/July14/setUp.sh
+SNDBUILD_DIR=/afs/cern.ch/user/s/snd2cern/public/SNDBUILD/sw
+source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Jan22/setUp.sh
 eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest-master-release`
-source /afs/cern.ch/work/a/aiulian/public/fedra/setup_new.sh
+source /afs/cern.ch/user/s/snd2cern/public/fedra/setup_new.sh
+
 
 echo  "go into reconstruction folder "
 cd /eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN1/b000021/
