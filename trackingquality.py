422b26af775ff3e79f1540909612b928f44dc3a5
aiulian <antonio.iuliano@cern.ch>
2023-06-29 14:29:18 +0200

    using seaborn and matplotlib for plots instead of deprecated root_numpy

--- trackingquality.py
+++ trackingquality.py
@@ -7,38 +7,22 @@
 # 
 # The functions were initially used in FOOT
 
-# In[2]:
+# usage: python trackingquality.py foldercontainingbricks
 
-
-import root_numpy
-import ROOT as r
-import rootnumpy_myutils
 import sys
-
-
-# In[3]:
 
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn #for regplot, equivalent as tprofile
 
-
-# In[4]:
-
+minnseg = 31
 
 simdf = pd.DataFrame()
 #brickIDs = [11,12,13,14,21,22,23,24,25,31,32,33,34,41,42,43,44,51,52,53,54]
 brickIDs = [11]
 
-
-# In[5]:
-
-
-#prepath = "/eos/user/a/aiulian/sim_fedra/SNDnuyield_17_November_2021"
 prepath = sys.argv[1]
-
-
-# In[6]:
 
 
 for brID in brickIDs:
@@ -48,22 +32,10 @@ for brID in brickIDs:
     simdf = pd.concat([simdf,singledf],axis=0)
 #simdf = pd.read_csv("/eos/user/a/aiulian/sim_fedra/SNDnuyield_17_November_2021/b000032/brick32.csv")
 
-
-# In[ ]:
-
-
-
-
-
-# In[7]:
-
-
 simdf.head()
 
 
 # Counting segments associated to the reconstructed track and to true MCTracks 
-
-# In[8]:
 
 
 nseg = simdf.groupby(["brickID","FEDRATrackID"]).count()["ID"] #number of segments associated to each reconstructed track
@@ -71,8 +43,6 @@ nsegsamemc = simdf.groupby(["brickID","FEDRATrackID","MCEvent","MCTrack"]).count
 
 
 # ## Information for tracked segment only
-
-# In[9]:
 
 
 #taking tracked segments
@@ -104,22 +74,19 @@ trackdf = trackdf.groupby(["brickID","FEDRATrackID"]).last()
 trackdf["MCEvent"] = [index[0] for index in mostfrequentpair]
 trackdf["MCTrack"] = [index[1] for index in mostfrequentpair]
 
-# In[10]:
-
 
 #how many reconstructed track for each true track
 nsplit = trackdf.groupby(["brickID","MCEvent","MCTrack"]).count()["ID"]
 
 
-# In[11]:
+#hsplit = r.TH1D("hsplit","nsplit",8,0,8)
+#root_numpy.fill_hist(hsplit,nsplit)
+#csplit = r.TCanvas()
+#hsplit.Draw()
 
-hsplit = r.TH1D("hsplit","nsplit",8,0,8)
-root_numpy.fill_hist(hsplit,nsplit)
-csplit = r.TCanvas()
-hsplit.Draw()
+plt.figure()
+plt.hist(nsplit.values, range = (0,8),bins=8)
 
-
-# In[12]:
 
 
 #adding computed information to our dataset
@@ -138,9 +105,6 @@ trackdf = trackdf.reset_index().merge(nsegtrue, how = "left", on = ["brickID","M
 trackdf = trackdf.merge(nsegsamemc, how = "left", on = ["brickID","FEDRATrackID","MCEvent","MCTrack"])
 
 
-# In[13]:
-
-
 #renaming labels
 trackdf["PID"] = trackdf["PID_x"]
 trackdf["ID"] = trackdf["ID_x"]
@@ -155,123 +119,69 @@ del trackdf["PID_x"]
 
 # ## computing efficiencies
 
-# In[14]:
-
 
 #computing efficiency
 trackdf["efficiency"] = trackdf["nsegsamemc"]/trackdf["nsegtrue"]
 
-
-# In[15]:
-
-
 trackdf.head()
 
 
-# In[16]:
+longtracks = trackdf.query("nsegtrue>{}".format(minnseg))
 
-
-longtracks = trackdf.query("nsegtrue>10")
-
-plt.hist(longtracks["efficiency"],bins=11,range=[0,1.1])
-
-
-# In[ ]:
-
-
-
+plt.figure()
+plt.hist(longtracks["efficiency"],bins=11,range=(0,1.1))
 
 
 # ## Using root_numpy libraries to fill ROOT histograms with numpy objects 
 # 
 # Let us start to make a TProfile angle vs efficiency
 
-# In[17]:
-
 
 import numpy as np
 longtracks["Theta"] = np.sqrt(longtracks["TX"]**2 + longtracks["TY"]**2)
 
 
-# In[18]:
+#profefftheta = r.TProfile("profefftheta","Efficiency for different theta angles;theta[rad];efficiency",11,0,1.1,0.,1.1)
 
+#rootnumpy_myutils.fillprofile2D(profefftheta,longtracks["Theta"],longtracks["efficiency"])
 
-profefftheta = r.TProfile("profefftheta","Efficiency for different theta angles;theta[rad];efficiency",11,0,1.1,0.,1.1)
+#cefftheta = r.TCanvas()
+#profefftheta.Draw()
+#cefftheta.Draw()
 
-rootnumpy_myutils.fillprofile2D(profefftheta,longtracks["Theta"],longtracks["efficiency"])
+plt.figure()
+seaborn.regplot(x=longtracks["Theta"],y=longtracks["efficiency"],x_bins=11,marker='o',fit_reg=False)
 
+#profeffP = r.TProfile("profP","Efficiency for different momenta;P[GeV/c];efficiency",20,0,1000.,0.,1.1)
 
-# In[19]:
+#rootnumpy_myutils.fillprofile2D(profeffP,longtracks["P"],longtracks["efficiency"])
 
-
-cefftheta = r.TCanvas()
-profefftheta.Draw()
-cefftheta.Draw()
-
-
-# In[20]:
-
-
-profeffP = r.TProfile("profP","Efficiency for different momenta;P[GeV/c];efficiency",20,0,1000.,0.,1.1)
-
-rootnumpy_myutils.fillprofile2D(profeffP,longtracks["P"],longtracks["efficiency"])
-
-
-# In[21]:
-
-
-ceffP = r.TCanvas()
-profeffP.Draw()
-ceffP.Draw()
-
-
-# In[22]:
-
-
-rootnumpy_myutils.fillprofile2D(profeffP,longtracks["P"],longtracks["efficiency"])
-
-
-# In[23]:
-
-
-longtracks.query("Theta<0.1").head()
-
-
-# In[24]:
-
-
-longtracks.query("MCEvent==10 and MCTrack==10")
-
-
-# In[25]:
-
-
-longtracks.query("MCEvent==10 and MCTrack==10")[["P","PdgCode"]]
-
-
-# In[26]:
-
+plt.figure()
+seaborn.regplot(x=longtracks["P"],y=longtracks["efficiency"],x_bins=20,marker='o',fit_reg=False)
+plt.title("Efficiency for different momenta of tracks with at least {}".format(minnseg))
 
 singletrackdf = trackdf.groupby(["brickID","MCEvent","MCTrack"]).first()
 singletrackdf["nsplit"] = nsplit
-profeffnsplit = r.TProfile("profnsplit","Efficiency for different nsplit;nsplit;efficiency",8,0,8,0.,1.1)
+#profeffnsplit = r.TProfile("profnsplit","Efficiency for different nsplit;nsplit;efficiency",8,0,8,0.,1.1)
 
-rootnumpy_myutils.fillprofile2D(profeffnsplit,singletrackdf["nsplit"],singletrackdf["efficiency"])
+#rootnumpy_myutils.fillprofile2D(profeffnsplit,singletrackdf["nsplit"],singletrackdf["efficiency"])
 
+plt.figure()
+seaborn.regplot(x=singletrackdf["nsplit"],y=singletrackdf["efficiency"],x_bins=8,marker='o',fit_reg=False)
+plt.title("Efficiency counted to single MCTrack")
+#ceffnsplit = r.TCanvas()
+#profeffnsplit.Draw()
+#ceffnsplit.Draw()
 
-# In[28]:
+#hsplit_mu = r.TH1D("hsplit_mu","nsplit",8,0,8)
+#root_numpy.fill_hist(hsplit_mu,singletrackdf.query("MCTrack==0")["nsplit"])
+#cnpslit_mu = r.TCanvas()
+#hsplit_mu.Draw()
 
-
-ceffnsplit = r.TCanvas()
-profeffnsplit.Draw()
-ceffnsplit.Draw()
-
-
-# In[ ]:
-hsplit_mu = r.TH1D("hsplit_mu","nsplit",8,0,8)
-root_numpy.fill_hist(hsplit_mu,singletrackdf.query("MCTrack==0")["nsplit"])
-cnpslit_mu = r.TCanvas()
-hsplit_mu.Draw()
+plt.figure()
+plt.hist(singletrackdf.query("MCTrack==0")["nsplit"].values, range = (0,8),bins=8)
+plt.title("nsplit for muon tracks")
+plt.show()
 
 
 
