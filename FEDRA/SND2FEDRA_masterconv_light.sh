c2503b9ead3639d8dc7c01e1fb7187f5b77c2d9d
Daniele Centanni <daniele.centanni@outlook.it>
2023-07-07 11:33:49 +0200

    Moved to lighter versions of the scripts

---
+++ FEDRA/SND2FEDRA_masterconv_light.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Bash script wrapping all of "FairShip2Fedra" conversion tools
+# you'll need to copy in this folder the sim file you want to
+# convert in FEDRA
+RED=$'\033[0;31m' #Red coloring                                                                                                                                                  
+NC=$'\033[0m' #No coloring
+
+SIMFILE=$(ls sndLHC.Ntuple-TGeant4-*.root)
+[ ! -d "$SIMFILE" ] && echo "${RED}++${NC} You are about to convert $SIMFILE ${RED}++${NC}"
+echo "${RED}++${NC} Bricks and plates folders will be created in the following directory: $(pwd) ${RED}++${NC}"
+cp /home/simsndlhc/macros-snd/FEDRA/preparebricks.sh .
+cp /home/simsndlhc/macros-snd/FEDRA/doreco.sh .
+cp /home/simsndlhc/macros-snd/FEDRA/csvconversion.sh .
+cp /home/simsndlhc/macros-snd/FEDRA/addvertexinfo.sh .
+cp /home/simsndlhc/macros-snd/FEDRA/fromsndsw2FEDRA.C .
+cp /home/simsndlhc/macros-snd/FEDRA/FairShip2Fedra.rootrc .
+cp /home/simsndlhc/macros-snd/GetEntries.C .
+for i in $(seq 1 5)
+	do
+		mkdir b0000${i}{1..4}
+		mkdir b0000${i}{1..4}/p0{01..60}
+	done
+echo "++++++++++++++++"
+echo "FairShip2Fedra parameters are the following:"
+cat FairShip2Fedra.rootrc
+for ibrick in $(seq 0 19)
+	do
+		root -l -q fromsndsw2FEDRA.C\(\"$SIMFILE\",$ibrick\)
+	done
+echo "${RED}++${NC} Proceeding to doreco.sh ${RED}++${NC}"
+source doreco.sh
+echo "${RED}++${NC} Done ${RED}++${NC}"
+echo "${RED}++${NC} Now performing csvconversion ${RED}++${NC}"
+source csvconversion.sh
+echo "${RED}++${NC} Done ${RED}++${NC}"
+echo "${RED}++${NC} Now adding vertex informations ${RED}++${NC}"
+source addvertexinfo.sh
+echo "${RED}++${NC} Merging all vertextree files ${RED}++${NC}"
+hadd vertextree_allbricks.root b0000*/vertextree.root
+echo "${RED}++${NC} SND2FEDRA conversion successfully ended ${RED}++${NC}"
--- FEDRA/doreco_sndsw.sh
+++ FEDRA/doreco_sndsw.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 SCRIPTFOLDER=$SNDSW_ROOT/shipLHC/scripts/fromsndsw2FEDRA/
 echo "There are two possible track.rootrc files in ${SCRIPTFOLDER} which one do you wish to use ?"
-select yn in "DEFAULT" "EXPERIMENTAL" "OTHER"; do
-	case $yn in
-		DEFAULT ) echo "You selected track.rootrc";cp $SCRIPTFOLDER/track.rootrc ./; break;;
-        EXPERIMENTAL ) echo "You selected track_experimental.rootrc";cp $SCRIPTFOLDER/track_experimental.rootrc ./track.rootrc;break;;
-	OTHER ) echo "You selected OTHER, please edit your track.rootrc file. Press enter to continue, q to abort";read -rsn1 -p $"" key
-                if [[ "$key" == "q" ]]
-                        then
-                                echo ""
-                                return
-                fi;break;;    
-	esac
-done
-cat track.rootrc
-read -n1 -r -s -p $"If it's ok for you press enter to continue, q to abort" key
-if [[ "$key" == "q" ]] 
-   then 
-   		echo ""
-   		return
-fi
+echo "Using default parameters for tracking, as listed here:"
+cp $SCRIPTFOLDER/track.rootrc ./
+cat ./track.rootrc
 brickIDs=(11 12 13 14 21 22 23 24 31 32 33 34 41 42 43 44 51 52 53 54) #ehi, I have written an array in BASH!
 for ibrick in $(seq 0 19)
  do
