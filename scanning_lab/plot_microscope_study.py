c8bde6368eb73080e09e72426863d19ea94a77f1
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2023-09-30 16:56:30 +0200

    setting time with datetime properly

--- scanning_lab/plot_microscope_study.py
+++ scanning_lab/plot_microscope_study.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 
-micdf = pd.read_csv("/home/antonio/cernbox/Synched/Archivio_cronologico/Febbraio_2023/scanning_table.csv")
+micdf = pd.read_csv("/home/utente/cernbox/Synched/Archivio_cronologico/Settembre_2023/CERN_scanningtable.csv")
 
 plt.figure()
 
-plt.plot(micdf["Week"],micdf["NScans"],"bo",markersize=10)
+DateWeek = pd.to_datetime(micdf["Week"])
+
+plt.plot(DateWeek,micdf["NScans"],"bo",markersize=10)
 
 plt.ylabel("NFilms Scanned x week",fontsize='x-large')
+
+plt.ylim([0,15])
+
+myFmt=mdates.DateFormatter('%m-%d')
+plt.gca().xaxis.set_major_formatter(myFmt)
 
 plt.show()
