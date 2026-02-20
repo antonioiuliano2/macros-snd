cde7987357adc3397ae42424e51c70eb99bba417
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2026-02-20 15:00:55 +0100

    removing stopwatch after speed tests

--- FEDRA/fast_poibin_combination_basetracks_efficiencies.py
+++ FEDRA/fast_poibin_combination_basetracks_efficiencies.py
@@ -4,7 +4,6 @@ from fast_poibin import PoiBin
 
 min_nfilms = 18
 
-stopwatch = r.TStopwatch()
 # note: with recent ROOT versions ROOT is even more Pythonic, see 
 # https://indico.cern.ch/event/1642215/contributions/6927591/attachments/3221142/5739996/260217_ROOT_Status_and_Plans_SHiP.pdf
 def GetEfficiencyArray(inputfile):
@@ -30,12 +29,12 @@ poibin = PoiBin(arr_eff)
 #printing results
 np.set_printoptions(precision=2) #for scientific notation
 print("Probability of having exactly k base-tracks, starting from {}".format(min_nfilms))
+
 #pmf is Probability Mass Function, i.e. the probability of having k successes over n trials
 print(poibin.pmf[min_nfilms:]*100)
 print("\n")
+
 #cdf is Cumulative Distribution Function, i.e. the probability of having at maximum k successes (included!)
-print("Probability of having at least {} base-tracks:\n{:.5}".format(min_nfilms, (1 -poibin.cdf[min_nfilms-1])*100))
+tracking_efficiency = 1 -poibin.cdf[min_nfilms-1]
+print("Probability of having at least {} base-tracks:\n{:.5}".format(min_nfilms, tracking_efficiency *100))
 print("\n")
-#just printing computation time info
-stopwatch.Stop()
-stopwatch.Print()
