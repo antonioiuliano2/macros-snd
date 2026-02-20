202ec27d8a86267930b8770a3420f40cc19167e2
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2026-02-20 14:41:08 +0100

    printing results

--- FEDRA/fast_poibin_combination_basetracks_efficiencies.py
+++ FEDRA/fast_poibin_combination_basetracks_efficiencies.py
@@ -19,19 +19,26 @@ def GetEfficiencyArray(inputfile):
      #get efficiency value
     
      geff = heff.GetPaintedGraph()
-     print(geff)
      arr_eff = np.array(geff.GetY())
      #remove zeros from not existing plates
      arr_eff = arr_eff[arr_eff != 0]
      
      return arr_eff
     
-    
-
+#get eff array
 arr_eff = GetEfficiencyArray("efficiency_plate_allquarters.root")
+#calling fast Poisson Binomial Distribution module, https://github.com/privet-kitty/fast-poibin/
 poibin = PoiBin(arr_eff)
 
-print(poibin.pmf)
-
+#printing results
+np.set_printoptions(precision=2) #for scientific notation
+print("Probability of having exactly k base-tracks, starting from {}".format(min_nfilms))
+#pmf is Probability Mass Function, i.e. the probability of having k successes over n trials
+print(poibin.pmf[min_nfilms:]*100)
+print("\n")
+#cdf is Cumulative Distribution Function, i.e. the probability of having at maximum k successes
+print("Probability of having at least {} base-tracks:\n{:.5}".format(min_nfilms, (1 -poibin.cdf[min_nfilms])*100))
+print("\n")
+#just printing computation time info
 stopwatch.Stop()
 stopwatch.Print()
