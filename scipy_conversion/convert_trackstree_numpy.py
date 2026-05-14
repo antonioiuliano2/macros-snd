3d6b2b327cefe737613f213641b125b0d4df4733
Antonio Iuliano <antonio.iuliano@na.infn.it>
2026-05-14 15:42:46 +0200

    fix: C++ code to treat branches with leaf size and static length

--- scipy_conversion/convert_trackstree_numpy.py
+++ scipy_conversion/convert_trackstree_numpy.py
@@ -2,14 +2,55 @@ import ROOT as r
 import pandas as pd
 import numpy as np
 
+# Define helper function in C++ to extract the updated RDataFrame with the new columns
+r.gInterpreter.Declare("""
+ROOT::RDF::RNode PreprocessTracks(ROOT::RDataFrame df) {
+    auto getFirst = [](const ROOT::RVec<int>& vec, const ROOT::RVec<int>& mcTrack) {
+        ROOT::RVec<int> res;
+        //start loop over the vec to copy values
+        for (size_t i = 0; i < vec.size(); i += 2) {
+            res.push_back(vec[i]);
+
+            if (i < vec.size() -1) {
+            //the second variable is always 0, we need to know if the MCTrack is the same or it changed
+             if (mcTrack[i+1] == mcTrack[i]) res.push_back(vec[i]);
+             else {
+              //the particle changed, we need to check if the next one is the same as the current one
+              if (i < vec.size() - 2){
+
+               if (mcTrack[i+1] == mcTrack[i+2]) res.push_back(vec[i+2]);
+               else res.push_back(vec[i]);
+
+              } //end if we have arrived to the second to last element
+              else res.push_back(vec[i]);
+             }
+
+            } //end if we have arrived to the last element
+         } //end for loop over vector
+        return res;
+    };
+    // Here in C++ s.eVid and s.eAid can be recognized
+    return df.Define("s_PdgCode", getFirst, {"s.eVid[2]","s.eMCTrack"}).Define("s_MotherId", getFirst, {"s.eAid[2]","s.eMCTrack"});
+}
+""")
+
+# In Python
+df_raw = r.RDataFrame("tracks", "b000021.0.0.97.trk.root")
+
+# Passiamo l'RDataFrame alla funzione C++
+# ROOT.RDF.RNode è il tipo generico per i nodi del grafo
+df_processed = r.PreprocessTracks(df_raw)
+
+# Ora puoi usarlo normalmente in Python
+
 #read the RNtuple
 #rdf = r.RDataFrame("tracks_NTuple","volumetracks_rdataframe.root")
 #read the TTree and convert it on the fly, no intermediate file
-rdf = r.RDataFrame("tracks","b000006.0.0.0.trk.root")
+rdf = r.RDataFrame("tracks","b000021.0.0.97.trk.root")
 #convert into a pandas dataframe
-df = pd.DataFrame(rdf.AsNumpy(columns = ["s.eID","s.ePID","s.eX","s.eY","s.eZ","s.eTX","s.eTY","s.eMCTrack","s.eP","s.eFlag","trid"]))
+df = pd.DataFrame(df_processed.Range(31,32).AsNumpy(columns = ["s.eID","s.ePID","s.eX","s.eY","s.eZ","s.eTX","s.eTY","s.eMCTrack","s.eP","s.eFlag","s_PdgCode","s_MotherId","trid"]))
 #renameing them to remove the s.e prefix
-df.columns = ["ID","PID","X","Y","Z","TX","TY","MCTrack","P","Flag","trid"]
+df.columns = ["ID","PID","X","Y","Z","TX","TY","MCTrack","P","Flag","PdgCode","MotherId","trid"]
 #explode into 1D (i.e. each row is a segment, not a track)
 #everything except trid, which is already one entry per track
-df_exploded = df.explode(["ID","PID","X","Y","Z","TX","TY","MCTrack","P","Flag"]) 
+df_exploded = df.explode(["ID","PID","X","Y","Z","TX","TY","MCTrack","P","Flag","PdgCode","MotherId"])
